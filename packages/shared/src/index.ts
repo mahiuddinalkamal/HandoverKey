a1863a8e4c20027873eba7296dd676d3aa1c9b97
Mahiuddin Al Kamal <mahiuddinalkamal@gmail.com>
2025-07-31 21:38:22 +0200

    Add dead man's switch database schema and types

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./types/crypto";
 export * from "./types/user";
 export * from "./types/api";
+export * from "./types/dead-mans-switch";
 export * from "./utils/validation";
 export * from "./utils/constants";
